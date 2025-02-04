edcdd6eb6404af52ef1c4cd62ca7675a860bf951
Nguyen Vinh Hung <52200097@student.tdtu.edu.vn>
2025-02-04 08:35:29 +0700

    modify Queue

--- Lab2_Stack&Queue/Queue/ArrayImplement/ArrayQueue.java
+++ Lab2_Stack&Queue/Queue/ArrayImplement/ArrayQueue.java
@@ -2,30 +2,30 @@ package Queue.ArrayImplement;
 import Queue.MyQueue;
 
 public class ArrayQueue<E> implements MyQueue<E> {
-    private int size = 0;
     private E[] data;
     private int front = 0;
     private int rear = -1;
     private int count = 0;
     @SuppressWarnings("unchecked")
     public ArrayQueue() {
-        this.size = 1000;
-        this.data = (E[]) new Object[this.size];
+        this.data = (E[]) new Object[1000];
+        this.front = 0;
+        this.rear = data.length - 1;
     }
     public E[] resize(int newSize) {
         @SuppressWarnings("unchecked")
         E[] newData = (E[]) new Object[newSize];
-        for (int i = 0; i < this.size; i++) {
+        for (int i = 0; i < data.length; i++) {
             newData[i] = this.data[i];
         }
         return newData;
     }
     public void enQueue(E item) {
-        if(count == this.size) {
-            this.size *= 2;
-            this.data = this.resize(this.size);
+        if(count == data.length) {
+            int size = data.length * 2;
+            this.data = resize(size);
         }
-        this.rear = (this.rear + 1) % this.size;
+        this.rear = (this.rear + 1) % data.length;
         this.data[this.rear] = item;
         count++;
     }
@@ -35,7 +35,7 @@ public class ArrayQueue<E> implements MyQueue<E> {
         }
         E item = this.data[this.front];
         this.data[this.front] = null;
-        this.front = (this.front + 1) % this.size;
+        this.front = (this.front + 1) % data.length;
         count--;
         return item;
     }
@@ -51,7 +51,10 @@ public class ArrayQueue<E> implements MyQueue<E> {
         return false;
     }
     public void print() {
-        for (int i = 0; i < this.count; i++) {
+        for (int i = 0; i < data.length; i++) {
+            if(this.data[i] == null) {
+                continue;
+            }
             System.out.print(this.data[i] + " ");
         }
         System.out.println();
@@ -64,5 +67,20 @@ public class ArrayQueue<E> implements MyQueue<E> {
             return null;
         }
         return this.data[this.front];
+    }
+    public static void main(String[] args) {
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        queue.enQueue(1);
+        queue.enQueue(2);
+        queue.enQueue(3);
+        queue.enQueue(4);
+        queue.enQueue(5);
+        queue.enQueue(6);
+        queue.enQueue(7);
+        queue.enQueue(8);
+        queue.enQueue(9);
+        queue.print();
+        queue.deQueue();
+        queue.print();
     }
 }
